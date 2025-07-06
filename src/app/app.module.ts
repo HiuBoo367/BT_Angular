e37add3cbc900e5bc826ab85947e661b57bf3107
HiuBoo367 <buihuyhieu367@gmail.com>
2025-07-06 09:55:37 +0700

    BT-Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopHeaderMenuComponent } from './top-header/top-header-menu/top-header-menu.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TopHeaderComponent,
+    TopHeaderMenuComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/top-header/top-header-menu/top-header-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-header-menu',
+  templateUrl: './top-header-menu.component.html',
+  styleUrls: ['./top-header-menu.component.css']
+})
+export class TopHeaderMenuComponent {
+
+}
