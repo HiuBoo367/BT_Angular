b2256217ece392b824db8bc1859c00abefdbce6a
HiuBoo367 <buihuyhieu367@gmail.com>
2025-07-07 17:06:34 +0700

    Hiu1.3 (07-07-2025)

--- src/app/header/header-menu/header-menu.component.ts
+++ src/app/header/header-menu/header-menu.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-menu.component.css']
 })
 export class HeaderMenuComponent {
-
+   menuItems: string[] = ['Tổng Quan', 'Sản Phẩm', 'Xuất Bán', 'Báo Cáo'];
 }
